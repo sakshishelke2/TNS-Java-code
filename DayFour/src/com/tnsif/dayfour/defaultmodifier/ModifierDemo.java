2187083a3c95646397e023441263d97710837fd5
Sakshi Shelke <sakshishelke12@gmail.com>
2025-09-01 22:52:05 +0530

    modifiers

---
+++ DayFour/src/com/tnsif/dayfour/defaultmodifier/ModifierDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.dayfour.defaultmodifier;
+
+import com.tnsif.dayfour.modifiers.Base;
+
+public class ModifierDemo extends Base{
+
+	Base b1 = new Base();
+	// b1.methodDefault(); will show error
+//	b1.methodProtected();
+	
+}
+
+
---
+++ DayFour/src/com/tnsif/dayfour/modifiers/Executor.java
@@ -0,0 +1,11 @@
+package com.tnsif.dayfour.modifiers;
+
+public class Executor {
+
+	// accessing same packagee class
+			Base b1 = new Base();
+		//	b1.methodPrivate();
+			b1.methodDefault();
+			b1.methodProtected();
+		}
+}
